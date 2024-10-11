72e56e5e1a7667dab0830ea82a0e32743072aab0
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-10-11 23:49:43 +0600

    Job card added

--- src/app/components/Home/Jobs.tsx
+++ src/app/components/Home/Jobs.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import vector from "../../../assets/Vector.png";
 
 const Jobs = () => {
   return (
@@ -20,6 +22,170 @@ const Jobs = () => {
         <p className="mt-1 text-center text-[22px] font-semibold text-[#4B5155BF] lg:mt-4">
           Pick the one that fits your skill set
         </p>
+      </div>
+
+      <div className="grid grid-cols-4 gap-2">
+
+        <div
+          style={{ boxShadow: `rgba(100, 100, 111, 0.2) 0px 7px 29px 0px` }}
+          className="h-[353px] w-[337px] rounded-xl px-[36.5px] py-[45px]"
+        >
+          <div className="flex items-center justify-between">
+            <Image
+              src={vector}
+              alt="Vector of Job's card"
+              height={63}
+              width={63}
+            ></Image>
+            <div className="ml-6 text-sm">
+              <h5 className="font-semibold text-[#184E77]">
+                Local Economies: A Case Study Approach
+              </h5>
+              <p className="font-medium text-[#4B5155]">Essay</p>
+            </div>
+          </div>
+          <hr className="my-4" style={{ border: "1.25px solid #4B51554D" }} />
+          <p className="text-[10px] text-[#4B5155]">
+            Hiring a writer to create an assignment on &quot;Exploring Local
+            Economies through Case Studies,&quot; focusing on research and
+            analysis of unique economic strategies
+          </p>
+          <hr className="my-4" style={{ border: "1.25px solid #4B51554D" }} />
+          <div className="flex justify-center">
+            <Link
+              href="#"
+              className="text-sm font-medium text-[#184E77] underline"
+            >
+              See Details
+            </Link>
+          </div>
+          <div className="flex justify-center">
+            <button className="rounded bg-[#184E77] px-4 py-2 text-xs font-semibold text-[#FFFFFF]">
+              Apply Now
+            </button>
+          </div>
+        </div>
+
+        <div
+          style={{ boxShadow: `rgba(100, 100, 111, 0.2) 0px 7px 29px 0px` }}
+          className="h-[353px] w-[337px] rounded-xl px-[36.5px] py-[45px]"
+        >
+          <div className="flex items-center justify-between">
+            <Image
+              src={vector}
+              alt="Vector of Job's card"
+              height={63}
+              width={63}
+            ></Image>
+            <div className="ml-6 text-sm">
+              <h5 className="font-semibold text-[#184E77]">
+                Local Economies: A Case Study Approach
+              </h5>
+              <p className="font-medium text-[#4B5155]">Essay</p>
+            </div>
+          </div>
+          <hr className="my-4" style={{ border: "1.25px solid #4B51554D" }} />
+          <p className="text-[10px] text-[#4B5155]">
+            Hiring a writer to create an assignment on &quot;Exploring Local
+            Economies through Case Studies,&quot; focusing on research and
+            analysis of unique economic strategies
+          </p>
+          <hr className="my-4" style={{ border: "1.25px solid #4B51554D" }} />
+          <div className="flex justify-center">
+            <Link
+              href="#"
+              className="text-sm font-medium text-[#184E77] underline"
+            >
+              See Details
+            </Link>
+          </div>
+          <div className="flex justify-center">
+            <button className="rounded bg-[#184E77] px-4 py-2 text-xs font-semibold text-[#FFFFFF]">
+              Apply Now
+            </button>
+          </div>
+        </div>
+
+        <div
+          style={{ boxShadow: `rgba(100, 100, 111, 0.2) 0px 7px 29px 0px` }}
+          className="h-[353px] w-[337px] rounded-xl px-[36.5px] py-[45px]"
+        >
+          <div className="flex items-center justify-between">
+            <Image
+              src={vector}
+              alt="Vector of Job's card"
+              height={63}
+              width={63}
+            ></Image>
+            <div className="ml-6 text-sm">
+              <h5 className="font-semibold text-[#184E77]">
+                Local Economies: A Case Study Approach
+              </h5>
+              <p className="font-medium text-[#4B5155]">Essay</p>
+            </div>
+          </div>
+          <hr className="my-4" style={{ border: "1.25px solid #4B51554D" }} />
+          <p className="text-[10px] text-[#4B5155]">
+            Hiring a writer to create an assignment on &quot;Exploring Local
+            Economies through Case Studies,&quot; focusing on research and
+            analysis of unique economic strategies
+          </p>
+          <hr className="my-4" style={{ border: "1.25px solid #4B51554D" }} />
+          <div className="flex justify-center">
+            <Link
+              href="#"
+              className="text-sm font-medium text-[#184E77] underline"
+            >
+              See Details
+            </Link>
+          </div>
+          <div className="flex justify-center">
+            <button className="rounded bg-[#184E77] px-4 py-2 text-xs font-semibold text-[#FFFFFF]">
+              Apply Now
+            </button>
+          </div>
+        </div>
+
+        <div
+          style={{ boxShadow: `rgba(100, 100, 111, 0.2) 0px 7px 29px 0px` }}
+          className="h-[353px] w-[337px] rounded-xl px-[36.5px] py-[45px]"
+        >
+          <div className="flex items-center justify-between">
+            <Image
+              src={vector}
+              alt="Vector of Job's card"
+              height={63}
+              width={63}
+            ></Image>
+            <div className="ml-6 text-sm">
+              <h5 className="font-semibold text-[#184E77]">
+                Local Economies: A Case Study Approach
+              </h5>
+              <p className="font-medium text-[#4B5155]">Essay</p>
+            </div>
+          </div>
+          <hr className="my-4" style={{ border: "1.25px solid #4B51554D" }} />
+          <p className="text-[10px] text-[#4B5155]">
+            Hiring a writer to create an assignment on &quot;Exploring Local
+            Economies through Case Studies,&quot; focusing on research and
+            analysis of unique economic strategies
+          </p>
+          <hr className="my-4" style={{ border: "1.25px solid #4B51554D" }} />
+          <div className="flex justify-center">
+            <Link
+              href="#"
+              className="text-sm font-medium text-[#184E77] underline"
+            >
+              See Details
+            </Link>
+          </div>
+          <div className="flex justify-center">
+            <button className="rounded bg-[#184E77] px-4 py-2 text-xs font-semibold text-[#FFFFFF]">
+              Apply Now
+            </button>
+          </div>
+        </div>
+        
       </div>
     </div>
   );
