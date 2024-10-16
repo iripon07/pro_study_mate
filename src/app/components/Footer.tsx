def303197bcbfedfc09b2771010277a225166097
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-10-17 00:52:05 +0600

    header updated

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -6,78 +6,78 @@ import Instagram from "../../assets/instagram.png";
 
 const Footer = () => {
   return (
-    <div className="bg-primary px-[53px] py-[58px] lg:py-10">
-      <div className="mx-auto hidden container md:block">
-        <div className="flex justify-between text-sm font-semibold text-white">
-          <div className="space-y-4">
-            <h6>Lorem ipsum</h6>
-            <h6>Lorem ipsum</h6>
-            <h6>Lorem ipsum</h6>
-            <h6>Lorem ipsum</h6>
-          </div>
-          <div className="space-y-4">
-            <h6>Lorem ipsum</h6>
-            <h6>Lorem ipsum</h6>
-            <h6>Lorem ipsum</h6>
-          </div>
-          <div className="space-y-4">
-            <h6>Lorem ipsum</h6>
-            <h6>Lorem ipsum</h6>
-            <h6>Lorem ipsum</h6>
-          </div>
-          <div className="space-y-4">
-            <h6>Lorem ipsum</h6>
-            <h6>Lorem ipsum</h6>
-            <h6>Lorem ipsum</h6>
-            <h6>Lorem ipsum</h6>
-          </div>
-          <div className="space-y-4">
-            <h6>Lorem ipsum</h6>
-            <h6>Lorem ipsum</h6>
-            <h6>Lorem ipsum</h6>
+    <div>
+      <div className="bg-primary px-[53px] py-[58px] lg:py-10">
+        <div className="container mx-auto hidden md:block">
+          <div className="flex justify-between text-sm font-semibold text-white">
+            <div className="space-y-4">
+              <h6>Lorem ipsum</h6>
+              <h6>Lorem ipsum</h6>
+              <h6>Lorem ipsum</h6>
+              <h6>Lorem ipsum</h6>
+            </div>
+            <div className="space-y-4">
+              <h6>Lorem ipsum</h6>
+              <h6>Lorem ipsum</h6>
+              <h6>Lorem ipsum</h6>
+            </div>
+            <div className="space-y-4">
+              <h6>Lorem ipsum</h6>
+              <h6>Lorem ipsum</h6>
+              <h6>Lorem ipsum</h6>
+            </div>
+            <div className="space-y-4">
+              <h6>Lorem ipsum</h6>
+              <h6>Lorem ipsum</h6>
+              <h6>Lorem ipsum</h6>
+              <h6>Lorem ipsum</h6>
+            </div>
+            <div className="space-y-4">
+              <h6>Lorem ipsum</h6>
+              <h6>Lorem ipsum</h6>
+              <h6>Lorem ipsum</h6>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="mx-auto mt-10 container">
-        <div className="flex flex-col items-center justify-between font-poppins text-xs font-bold text-[#FFFFFF] lg:flex-row">
-          <h6 className="order-2 lg:order-1 text-center">
-            Copyright © 2024 Pro Study Mate. All rights reserved.
-          </h6>
+        <div className="container mx-auto mt-10">
+          <div className="flex flex-col items-center justify-between font-poppins text-xs font-bold text-[#FFFFFF] lg:flex-row">
+            <h6 className="order-2 text-center lg:order-1">
+              Copyright © 2024 Pro Study Mate. All rights reserved.
+            </h6>
 
-          <div
-            className="order-1 mb-4 flex gap-6 lg:order-2 lg:mb-0"
-          >
-            <a href="https://www.google.com/" target="_blank">
+            <div className="order-1 mb-4 flex gap-6 lg:order-2 lg:mb-0">
+              <a href="https://www.google.com/" target="_blank">
+                <Image
+                  src={Google}
+                  width={36}
+                  height={36}
+                  alt="social icon of Google"
+                ></Image>
+              </a>
               <Image
-                src={Google}
+                src={Facebook}
+                className=""
+                width={47}
+                height={36}
+                alt="social icon of Facebook"
+              ></Image>
+              <Image
+                src={Gmail}
                 width={36}
                 height={36}
-                alt="social icon of Google"
+                alt="social icon of Gmail"
               ></Image>
-            </a>
-            <Image
-              src={Facebook}
-              className=""
-              width={47}
-              height={36}
-              alt="social icon of Facebook"
-            ></Image>
-            <Image
-              src={Gmail}
-              width={36}
-              height={36}
-              alt="social icon of Gmail"
-            ></Image>
-            <Image
-              src={Instagram}
-              width={36}
-              height={36}
-              alt="social icon of Instagram"
-            ></Image>
-          </div>
+              <Image
+                src={Instagram}
+                width={36}
+                height={36}
+                alt="social icon of Instagram"
+              ></Image>
+            </div>
 
-          <h6 className="order-3">Terms of use / Privacy policy</h6>
+            <h6 className="order-3">Terms of use / Privacy policy</h6>
+          </div>
         </div>
       </div>
     </div>
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -12,66 +12,65 @@ const Header = () => {
   const pathname = usePathname();
   console.log("Path name ", pathname);
   return (
-    <nav
-      style={{ boxShadow: "0px 4px 15px 2px rgba(24, 78, 119, 0.05)" }}
-      className="mx-auto max-w-[1512px]"
-    >
-      <div className="flex h-[160px] items-center justify-between px-6">
-        <Image
-          src={Hamburger}
-          alt="Hamburger"
-          className="block md:hidden"
-        ></Image>
-
-        <Link href="/" className="">
+    <div style={{ boxShadow: "0px 4px 15px 2px rgba(24, 78, 119, 0.05)" }}>
+      <nav className="mx-auto container">
+        <div className="flex h-[160px] items-center justify-between px-6">
           <Image
-            src={logo}
-            alt="Pro Study Mate Logo"
-            width={178}
-            height={63}
+            src={Hamburger}
+            alt="Hamburger"
+            className="block md:hidden"
           ></Image>
-        </Link>
-        <div className="hidden gap-6 font-poppins text-base font-semibold text-[#4B5155BF] md:flex">
-          <Link
-            className={`link ${pathname === "/" ? "text-primary" : "hover:text-primary"}`}
-            href="/"
-          >
-            Home
-          </Link>
-          <Link
-            className={`link ${pathname === "/job" ? "text-[17px] text-primary" : "hover:text-primary"}`}
-            href="/job"
-          >
-            Job
-          </Link>
-          <Link className="hover:text-primary" href="/student">
-            Student Room
-          </Link>
-          <Link className="hover:text-primary" href="/workshop">
-            Workshop
-          </Link>
-          <Link className="hover:text-primary" href="/">
-            About Us
-          </Link>
-        </div>
 
-        <div className="flex gap-6 text-center">
-          <Link
-            href="/sign"
-            className="hidden min-w-[115px] rounded border-2 border-primary bg-primary px-4 py-2 text-sm font-semibold text-[#FFFFFF] md:block"
-          >
-            Sign Up
+          <Link href="/" className="">
+            <Image
+              src={logo}
+              alt="Pro Study Mate Logo"
+              width={178}
+              height={63}
+            ></Image>
           </Link>
-          <Link
-            href="/login"
-            className="hidden min-w-[115px] rounded border-2 border-primary px-4 py-2 text-sm font-semibold text-primary transition duration-300 ease-in-out hover:bg-primary hover:text-white md:block"
-          >
-            Log In
-          </Link>
-          <Image src={User} alt="User" className="block md:hidden"></Image>
+          <div className="hidden gap-6 font-poppins text-base font-semibold text-[#4B5155BF] md:flex">
+            <Link
+              className={`link ${pathname === "/" ? "text-primary" : "hover:text-primary"}`}
+              href="/"
+            >
+              Home
+            </Link>
+            <Link
+              className={`link ${pathname === "/job" ? "text-[17px] text-primary" : "hover:text-primary"}`}
+              href="/job"
+            >
+              Job
+            </Link>
+            <Link className="hover:text-primary" href="/student">
+              Student Room
+            </Link>
+            <Link className="hover:text-primary" href="/workshop">
+              Workshop
+            </Link>
+            <Link className="hover:text-primary" href="/">
+              About Us
+            </Link>
+          </div>
+
+          <div className="flex gap-6 text-center">
+            <Link
+              href="/sign"
+              className="hidden min-w-[115px] rounded border-2 border-primary bg-primary px-4 py-2 text-sm font-semibold text-[#FFFFFF] md:block"
+            >
+              Sign Up
+            </Link>
+            <Link
+              href="/login"
+              className="hidden min-w-[115px] rounded border-2 border-primary px-4 py-2 text-sm font-semibold text-primary transition duration-300 ease-in-out hover:bg-primary hover:text-white md:block"
+            >
+              Log In
+            </Link>
+            <Image src={User} alt="User" className="block md:hidden"></Image>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
 
