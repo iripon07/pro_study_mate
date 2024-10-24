941147f17a958fe3d2ab3cb1e074b9afc53ac8e8
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-10-24 12:56:39 +0600

    Sign Up form Social icon added

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -17,8 +17,10 @@ const SignUp = () => {
             We're always looking for talented Mentors
           </h6>
 
-          <div className="mt-[26px] flex gap-4">
-            <div className="mb-[68px] flex w-full items-center justify-start rounded border-[1px] border-[#184E7780] py-[6px] pl-3">
+          <div
+            className="mt-[26px] flex gap-4"
+          >
+            <div className="flex w-full items-center justify-start rounded border-[1px] border-[#184E7780] py-[6px] pl-3">
               <div className="rounded-[10px] bg-[#E9EBED] px-3 py-[9px]">
                 <Image src={facebook} alt="Facebook Icon"></Image>
               </div>
@@ -26,8 +28,13 @@ const SignUp = () => {
                 Continue with Facebook
               </h6>
             </div>
-            <div className="mb-[18px] flex w-full items-center rounded bg-blue py-[6px] text-base font-semibold text-white">
-              <div className="ml-3 mr-6 rounded-2xl bg-white px-[9px] py-[4px]">
+
+            <div
+              className="flex w-full items-center rounded bg-blue text-base font-semibold text-white"
+            >
+              <div
+                className="ml-3 mr-6 rounded-2xl bg-white px-[9px] py-[4px]"
+              >
                 <Image src={googleIcon} alt="Google Icon "></Image>
               </div>
               <h6>Continue with Google</h6>
@@ -37,7 +44,9 @@ const SignUp = () => {
           <div className="my-[38px] flex items-center justify-center">
             {/* <div className="border-[1.25px] border-[#4B51554D]"></div> */}
             <hr className="w-full border-[1.25px] border-[#4B51554D]" />
-            <span className="px-3 font-poppins font-[275] text-base text-grey">OR</span>
+            <span className="px-3 font-poppins text-base font-[275] text-grey">
+              OR
+            </span>
             <hr className="w-full border-[1.25px] border-[#4B51554D]" />
           </div>
         </div>
