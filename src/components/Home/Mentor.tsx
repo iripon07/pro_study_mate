e2d5255963ea168373205ec7a0abb39bfe6794d4
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-11-07 19:21:05 +0600

    Mentor responsive

--- src/components/Home/Mentor.tsx
+++ src/components/Home/Mentor.tsx
@@ -1,130 +1,167 @@
+"use client";
+
 import Image from "next/image";
 import mentor from "@/assets/Mentor.png";
 import right from "@/assets/right.png";
 import rightArrow from "@/assets/rightArrow.png";
 import Review from "@/assets/Review.png";
+import "swiper/css";
+import "swiper/css/autoplay";
+import { Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 const Mentor = () => {
   return (
-    <div className="container mx-auto my-[72px]">
-      <div className="mb-[80px] text-center text-grey">
-        <h2 className="mb-2 text-[40px] font-medium">Meet Our Top Mentors</h2>
-        <p className="text-xl font-normal">
+    <div className="container mx-auto my-[72px] px-4">
+      <div className="xl:mb-[80px] md:mb-14 mb-10 text-center text-grey">
+        <h2 className="mb-2 xl:text-[40px] lg:text-4xl text-3xl font-medium">Meet Our Top Mentors</h2>
+        <p className="xl:text-xl md:text-lg text-base  font-normal">
           Experts Who’ve Guided Countless Students to Success
         </p>
       </div>
 
-      <div className="grid grid-cols-3 gap-[83px]">
+      <Swiper
+        spaceBetween={30}
+        loop={true}
+        modules={[Autoplay]}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 2,
+          },
+          1024: {
+            slidesPerView: 3,
+          },
+        }}
+        className="mySwiper"
+      >
 
-        <div className="rounded-t-[20px]">
-          <Image
-            className="w-full"
-            src={mentor}
-            alt="Photo of Mentor"
-            quality={100}
-          ></Image>
+        <SwiperSlide>
+          <div className="rounded-t-[20px]">
+            <Image
+              className="w-full"
+              src={mentor}
+              alt="Photo of Mentor"
+              quality={100}
+            ></Image>
 
-          <p className="mb-3 mt-5 text-xl font-medium text-darkBlue">
-            University of Wollongong
-          </p>
+            <p className="mb-3 mt-5 text-xl font-medium text-darkBlue">
+              University of Wollongong
+            </p>
 
-          <div className="flex items-center">
-            <p className="mr-6 text-[32px] font-bold text-black">Jullian H</p>
-            <Image src={right} alt="Right"></Image>
+            <div className="flex items-center">
+              <p className="mr-6 text-[32px] font-bold text-black">Jullian H</p>
+              <Image src={right} alt="Right"></Image>
+            </div>
+
+            <p className="mb-6 mt-[10px] text-xs font-light text-black">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, 
+            </p>
+
+            <Image src={Review} alt="Review"></Image>
+
+            <div className="flex items-center justify-between">
+              <p className="text-xl font-normal text-[#777777]">
+                Starting from
+              </p>
+              <p className="text-[40px] font-bold text-darkBlue">$30</p>
+            </div>
+
+            <button className="mt-6 flex w-full items-center justify-between bg-[#295FCC] px-10 py-1 text-xl font-semibold text-white hover:opacity-80">
+              Book me for your task
+              <Image src={rightArrow} alt="Right Arrow"></Image>
+            </button>
           </div>
+        </SwiperSlide>
 
-          <p className="mb-6 mt-[10px] text-xs font-light text-black">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, 
-          </p>
+        <SwiperSlide>
+          <div className="rounded-t-[20px]">
+            <Image
+              className="w-full"
+              src={mentor}
+              alt="Photo of Mentor"
+              quality={100}
+            ></Image>
 
-          <Image src={Review} alt="Review"></Image>
+            <p className="mb-3 mt-5 text-xl font-medium text-darkBlue">
+              University of Wollongong
+            </p>
 
-          <div className="flex items-center justify-between">
-            <p className="text-xl font-normal text-[#777777]">Starting from</p>
-            <p className="text-[40px] font-bold text-darkBlue">$30</p>
+            <div className="flex items-center">
+              <p className="mr-6 text-[32px] font-bold text-black">Jullian H</p>
+              <Image src={right} alt="Right"></Image>
+            </div>
+
+            <p className="mb-6 mt-[10px] text-xs font-light text-black">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, 
+            </p>
+
+            <Image src={Review} alt="Review"></Image>
+
+            <div className="flex items-center justify-between">
+              <p className="text-xl font-normal text-[#777777]">
+                Starting from
+              </p>
+              <p className="text-[40px] font-bold text-darkBlue">$30</p>
+            </div>
+
+            <button className="mt-6 flex w-full items-center justify-between bg-[#295FCC] px-10 py-1 text-xl font-semibold text-white hover:opacity-80">
+              Book me for your task
+              <Image src={rightArrow} alt="Right Arrow"></Image>
+            </button>
           </div>
+        </SwiperSlide>
 
-          <button className="mt-6 flex w-full items-center justify-between bg-[#295FCC] px-10 py-1 text-xl font-semibold text-white hover:opacity-80">
-            Book me for your task
-            <Image src={rightArrow} alt="Right Arrow"></Image>
-          </button>
-        </div>
+        <SwiperSlide>
+          <div className="rounded-t-[20px]">
+            <Image
+              className="w-full"
+              src={mentor}
+              alt="Photo of Mentor"
+              quality={100}
+            ></Image>
 
-        <div className="rounded-t-[20px]">
-          <Image
-            className="w-full"
-            src={mentor}
-            alt="Photo of Mentor"
-            quality={100}
-          ></Image>
+            <p className="mb-3 mt-5 text-xl font-medium text-darkBlue">
+              University of Wollongong
+            </p>
 
-          <p className="mb-3 mt-5 text-xl font-medium text-darkBlue">
-            University of Wollongong
-          </p>
+            <div className="flex items-center">
+              <p className="mr-6 text-[32px] font-bold text-black">Jullian H</p>
+              <Image src={right} alt="Right"></Image>
+            </div>
 
-          <div className="flex items-center">
-            <p className="mr-6 text-[32px] font-bold text-black">Jullian H</p>
-            <Image src={right} alt="Right"></Image>
+            <p className="mb-6 mt-[10px] text-xs font-light text-black">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, 
+            </p>
+
+            <Image src={Review} alt="Review"></Image>
+
+            <div className="flex items-center justify-between">
+              <p className="text-xl font-normal text-[#777777]">
+                Starting from
+              </p>
+              <p className="text-[40px] font-bold text-darkBlue">$30</p>
+            </div>
+
+            <button className="mt-6 flex w-full items-center justify-between bg-[#295FCC] px-10 py-1 text-xl font-semibold text-white hover:opacity-80">
+              Book me for your task
+              <Image src={rightArrow} alt="Right Arrow"></Image>
+            </button>
           </div>
-
-          <p className="mb-6 mt-[10px] text-xs font-light text-black">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, 
-          </p>
-
-          <Image src={Review} alt="Review"></Image>
-
-          <div className="flex items-center justify-between">
-            <p className="text-xl font-normal text-[#777777]">Starting from</p>
-            <p className="text-[40px] font-bold text-darkBlue">$30</p>
-          </div>
-
-          <button className="mt-6 flex w-full items-center justify-between bg-[#295FCC] px-10 py-1 text-xl font-semibold text-white hover:opacity-80">
-            Book me for your task
-            <Image src={rightArrow} alt="Right Arrow"></Image>
-          </button>
-        </div>
-
-        <div className="rounded-t-[20px]">
-          <Image
-            className="w-full"
-            src={mentor}
-            alt="Photo of Mentor"
-            quality={100}
-          ></Image>
-
-          <p className="mb-3 mt-5 text-xl font-medium text-darkBlue">
-            University of Wollongong
-          </p>
-
-          <div className="flex items-center">
-            <p className="mr-6 text-[32px] font-bold text-black">Jullian H</p>
-            <Image src={right} alt="Right"></Image>
-          </div>
-
-          <p className="mb-6 mt-[10px] text-xs font-light text-black">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, 
-          </p>
-
-          <Image src={Review} alt="Review"></Image>
-
-          <div className="flex items-center justify-between">
-            <p className="text-xl font-normal text-[#777777]">Starting from</p>
-            <p className="text-[40px] font-bold text-darkBlue">$30</p>
-          </div>
-
-          <button className="mt-6 flex w-full items-center justify-between bg-[#295FCC] px-10 py-1 text-xl font-semibold text-white hover:opacity-80">
-            Book me for your task
-            <Image src={rightArrow} alt="Right Arrow"></Image>
-          </button>
-        </div>
-        
-      </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
