5e52f1318e3dc709b521232772fa24360432e937
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-11-07 23:08:55 +0600

    Mentor and Workshop

--- src/components/Home/Mentor.tsx
+++ src/components/Home/Mentor.tsx
@@ -13,9 +13,11 @@ import { Swiper, SwiperSlide } from "swiper/react";
 const Mentor = () => {
   return (
     <div className="container mx-auto my-[72px] px-4">
-      <div className="xl:mb-[80px] md:mb-14 mb-10 text-center text-grey">
-        <h2 className="mb-2 xl:text-[40px] lg:text-4xl text-3xl font-medium">Meet Our Top Mentors</h2>
-        <p className="xl:text-xl md:text-lg text-base  font-normal">
+      <div className="mb-10 text-center text-grey md:mb-14 xl:mb-[80px]">
+        <h2 className="mb-2 text-3xl font-medium lg:text-4xl xl:text-[40px]">
+          Meet Our Top Mentors
+        </h2>
+        <p className="text-base font-normal md:text-lg xl:text-xl">
           Experts Who’ve Guided Countless Students to Success
         </p>
       </div>
@@ -41,7 +43,6 @@ const Mentor = () => {
         }}
         className="mySwiper"
       >
-
         <SwiperSlide>
           <div className="rounded-t-[20px]">
             <Image
@@ -161,6 +162,47 @@ const Mentor = () => {
             </button>
           </div>
         </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="rounded-t-[20px]">
+            <Image
+              className="w-full"
+              src={mentor}
+              alt="Photo of Mentor"
+              quality={100}
+            ></Image>
+
+            <p className="mb-3 mt-5 text-xl font-medium text-darkBlue">
+              University of Wollongong
+            </p>
+
+            <div className="flex items-center">
+              <p className="mr-6 text-[32px] font-bold text-black">Jullian H</p>
+              <Image src={right} alt="Right"></Image>
+            </div>
+
+            <p className="mb-6 mt-[10px] text-xs font-light text-black">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, 
+            </p>
+
+            <Image src={Review} alt="Review"></Image>
+
+            <div className="flex items-center justify-between">
+              <p className="text-xl font-normal text-[#777777]">
+                Starting from
+              </p>
+              <p className="text-[40px] font-bold text-darkBlue">$30</p>
+            </div>
+
+            <button className="mt-6 flex w-full items-center justify-between bg-[#295FCC] px-10 py-1 text-xl font-semibold text-white hover:opacity-80">
+              Book me for your task
+              <Image src={rightArrow} alt="Right Arrow"></Image>
+            </button>
+          </div>
+        </SwiperSlide>
+        
       </Swiper>
     </div>
   );
--- src/components/Home/Workshop.tsx
+++ src/components/Home/Workshop.tsx
@@ -12,12 +12,12 @@ import { Swiper, SwiperSlide } from "swiper/react";
 const Workshop = () => {
   return (
     <div className="bg-whiteSmoke">
-      <div className="container mx-auto py-9 px-4">
-        <div className="xl:mb-[110px] md:mb-10 mb-6  text-center">
-          <h2 className="xl:text-[40px] md:text-3xl text-2xl font-medium text-[#1E1E1E]">
+      <div className="container mx-auto px-4 py-9">
+        <div className="mb-6 text-center md:mb-10 xl:mb-[110px]">
+          <h2 className="text-2xl font-medium text-[#1E1E1E] md:text-3xl xl:text-[40px]">
             Latest Workshop
           </h2>
-          <p className="xl:text-xl md:text-lg text-base font-normal text-[#4B5155BF]">
+          <p className="text-base font-normal text-[#4B5155BF] md:text-lg xl:text-xl">
             Join Our Interactive Workshops for Skills, Strategies, and Success
           </p>
         </div>
@@ -157,7 +157,34 @@ const Workshop = () => {
               </div>
             </div>
           </SwiperSlide>
+          
+          <SwiperSlide>
+            <div className="rounded-b-[5px] rounded-t-[20px] bg-white">
+              <Image className="w-full" src={bigStock} alt="Big Stock"></Image>
+              <p className="mt-3 pl-4 text-base font-normal text-black">
+                #CSCIT883, Computer Vision and algorithm eiuye ertie tieye teit
+                yerte ..
+              </p>
 
+              <div className="my-5 ml-[46px] flex items-center justify-start rounded-l-full bg-whiteSmoke py-2 pl-4 text-[15px] font-normal text-[#FB6107]">
+                <GoClock />
+                <p className="ml-2">12 PM, November , 29, 2024 </p>
+              </div>
+
+              <div className="flex items-center justify-between pb-6 pl-4 pr-[22px]">
+                <div className="flex gap-2">
+                  <Image src={profile} alt="Profile"></Image>
+                  <div className="">
+                    <p className="text-[15px] text-[#4B5155]">
+                      B h m riaz uddin
+                    </p>
+                    <p>Reviews</p>
+                  </div>
+                </div>
+                <p className="text-xl font-normal text-black">$ 10</p>
+              </div>
+            </div>
+          </SwiperSlide>
         </Swiper>
       </div>
     </div>
