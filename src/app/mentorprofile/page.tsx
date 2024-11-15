dd868cf8aa1641a8565ebb39ba39b1a41971300c
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-11-15 14:21:27 +0600

    Mentor profile course card work with added

--- src/app/mentorprofile/page.tsx
+++ src/app/mentorprofile/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import MentorAvatar from "@/assets/MentorAvatar.png";
+import MentorWorkshop from "@/assets/MentorWorkshop.png";
+import Okay from "@/assets/Okay.png";
+import Review from "@/assets/Review.png";
 import Image from "next/image";
 import { CiCalendar, CiPaperplane } from "react-icons/ci";
 import { FaRegHeart, FaRegStar } from "react-icons/fa";
@@ -9,17 +12,15 @@ import { IoLocationOutline } from "react-icons/io5";
 import { MdOutlineEmail } from "react-icons/md";
 import { RiGalleryLine } from "react-icons/ri";
 import { TbWorld } from "react-icons/tb";
-import MentorWorkshop from "@/assets/MentorWorkshop.png";
-import Review from "@/assets/Review.png"
-import Okay from "@/assets/Okay.png"
+import graduatedCap from "@/assets/graduatedCap.png"
 
 const MentorProfile = () => {
   return (
-    <div className="bg-gray-100">
-      <div className="container mx-auto mt-5 px-6">
-        <div className="grid grid-cols-3 gap-4">
-          <div className="col-span-2 w-full">
-            <div className="font-inter mb-6 flex w-full justify-between rounded-sm bg-white px-4 py-3">
+    <div className="bg-gray-100 pt-5">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-12 gap-4">
+          <div className="col-span-7 w-full">
+            <div className="mb-6 flex w-full justify-between rounded-sm bg-white px-4 py-3 font-inter">
               <div className="flex gap-[22px]">
                 <Image src={MentorAvatar} alt="Mentor Avatar" />
                 <div className="mb-[6px]">
@@ -119,13 +120,11 @@ const MentorProfile = () => {
               </p>
               <div>
                 <p className="px-1 font-prosto text-sm text-[#777777]">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo
-                  reprehenderit, illo optio hic iure, tempore, veniam cumque
-                  corrupti esse veritatis voluptatum iusto temporibus aspernatur
-                  ex saepe molestiae nisi atque totam blanditiis eum harum quod!
-                  Repudiandae nihil aliquid beatae, sit veritatis consequuntur
-                  iure provident eaque! Repudiandae inventore dolores aut culpa?
-                  Repellendus nemo dolorem cumque voluptatibus recusandae ut{" "}
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen boo k. It has survived not only five centuries
                   <br /> <br />
                   voluptate voluptatum in sit deserunt deleniti explicabo at
                   illo soluta aut, tenetur architecto cum fugit. Adipisci ad
@@ -133,38 +132,109 @@ const MentorProfile = () => {
                   fugiat vero inventore in ut beatae harum aspernatur suscipit
                   officia accusantium corrupti, voluptatum deserunt ipsa
                   aliquam. Libero, debitis sint architecto beatae tempora
-                  incidunt odio nobis esse placeat quis doloribus nam reiciendis
-                  iusto expedita adipisci obcaecati consequuntur porro, eum
-                  consequatur, nihil error? Eum a, quaerat ut explicabo cum,
-                  repudiandae amet laborum earum dolor reprehenderit voluptate
-                  vero minus magni laudantium molestiae quas, obcaecati corporis
-                  dolores! Quo quae libero deserunt maxime corporis!
                 </p>
+              </div>
+            </div>
+
+            <div className="mt-[9px] bg-white p-4">
+              <div className="flex items-center justify-start gap-x-[6px]">
+                <Image src={graduatedCap} alt="Graduated Cap" quality={100} />
+                <p className="font-prosto text-black">
+                  Universities I Work With
+                </p>
+              </div>
+              <div className="mt-3 flex gap-x-4 pl-1 font-poppins text-sm font-normal text-[#4585F4]">
+                <p>University of Wollongong, Australia, </p>
+                <p>University of Wollongong, Australia, </p>
+                <p>University of Sydney</p>
               </div>
             </div>
           </div>
 
-          <div className="w-full">
+          <div className="col-span-5 w-full">
             <p className="text-base font-medium text-black">
               Up Coming Workshop
             </p>
 
-            <div>
+            <div className="">
+              <div className="mb-4 flex gap-4 bg-[#FFFFFF] p-2 font-prosto">
+                <Image src={MentorWorkshop} alt="Mentor Workshop" />
+
+                <div className="flex w-full justify-between">
+                  <div>
+                    <p className="mb-2 text-2xs text-[#C55818]">
+                      SUN NOV 3,2024, 12.30 PM
+                    </p>
+                    <div>
+                      <p className="text-xs text-black">
+                        CSE 101 Workshop: Exam Topic Cover
+                      </p>
+                      <p>$30</p>
+                    </div>
+                    <p className="text-2xs text-[#4B5155BF]">
+                      Organised by: Riaz Uddin
+                    </p>
+
+                    <Image src={Review} alt="Review" />
+                    <div className="flex items-center gap-[11px]">
+                      <Image
+                        src={Okay}
+                        alt="Okay"
+                        className="h-[18px] w-[18px]"
+                      />
+                      <p className="text-2xs text-[#4B5155BF]">
+                        5 Students Booked
+                      </p>
+                    </div>
+                  </div>
+                  <div className="grid grid-rows-[1fr_auto] p-4">
+                    <div>
+                      <p className="font-prosto text-xs text-black">$30</p>
+                    </div>
+                    <button className="font-poppins text-xs font-medium text-[#098C00] duration-300 hover:opacity-80">
+                      Buy Now
+                    </button>
+                  </div>
+                </div>
+              </div>
+
               <div className="flex gap-4 bg-[#FFFFFF] p-2 font-prosto">
                 <Image src={MentorWorkshop} alt="Mentor Workshop" />
-                <div>
-                  <p className="mb-2 text-2xs text-[#C55818]">
-                    SUN NOV 3,2024, 12.30 PM
-                  </p>
-                  <p className="text-xs text-black">
-                    CSE 101 Workshop: Exam Topic Cover
-                  </p>
-                  <p className="text-2xs text-[#4B5155BF]">
-                    Organised by: Riaz Uddin
-                  </p>
+
+                <div className="flex w-full justify-between">
                   <div>
-                    <Image src={Okay} alt="Okay"/>
-                    <p>5 Students Booked</p>
+                    <p className="mb-2 text-2xs text-[#C55818]">
+                      SUN NOV 3,2024, 12.30 PM
+                    </p>
+                    <div>
+                      <p className="text-xs text-black">
+                        CSE 101 Workshop: Exam Topic Cover
+                      </p>
+                      <p>$30</p>
+                    </div>
+                    <p className="text-2xs text-[#4B5155BF]">
+                      Organised by: Riaz Uddin
+                    </p>
+
+                    <Image src={Review} alt="Review" />
+                    <div className="flex items-center gap-[11px]">
+                      <Image
+                        src={Okay}
+                        alt="Okay"
+                        className="h-[18px] w-[18px]"
+                      />
+                      <p className="text-2xs text-[#4B5155BF]">
+                        5 Students Booked
+                      </p>
+                    </div>
+                  </div>
+                  <div className="grid grid-rows-[1fr_auto] p-4">
+                    <div>
+                      <p className="font-prosto text-xs text-black">$30</p>
+                    </div>
+                    <button className="font-poppins text-xs font-medium text-[#098C00] duration-300 hover:opacity-80">
+                      Buy Now
+                    </button>
                   </div>
                 </div>
               </div>
