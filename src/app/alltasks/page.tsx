74acdcc99c16fa98cf2a0196013e56d5f26f1eac
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-11-23 17:03:38 +0600

    all task

---
+++ src/app/alltasks/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const AllTasks = () => {
+  return (
+    <div className="container mx-auto font-poppins">
+      <div>
+        <p className="text-[32px] font-medium text-black mb-[2px]">
+          What are you looking For?
+        </p>
+        <p className="text-base text-grey font-light">Search by keywords, mentor, location, university & filters</p>
+      </div>
+    </div>
+  );
+};
+
+export default AllTasks;
